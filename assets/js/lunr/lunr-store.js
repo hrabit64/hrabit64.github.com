81f72219eb114afc324910c52bcb49551d414f29
hrabit <hzser123@gmail.com>
2021-05-10 16:46:47 +0900

    test

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "유니티 ML_Agents 공부해보기 - 기본구조(1) Agent",
+        "excerpt":"0. 개요 이 블로그의 모든 글은 “텐서플로와 유니티 ML-Agents로 배우는 강화학습” 도서를 공부하며 정리한 내용들이다. 대부분의 내용들과 코드들은 이 책을 정리한 것이다. 또한 이런 좋은 책을 내주신 저자분들께 감사드린다. 도서 링크: https://wikibook.co.kr/tensorflow-mlagents/ 기본적으로 강화학습 알고리즘을 이용해 개발을 하기 위해서는 알고리즘을 구현하는 것도 중요하지만, 알고리즘을 학습할 수 있는 환경을 구성하는 것도...","categories": ["ML_Agents"],
+        "tags": ["ML_Agents","Unity"],
+        "url": "/ml_agents/mlagent1/",
+        "teaser": null
+      }]
