e15e3a11467ac153634919ccbb324aaf0ceddc5d
hrabit <hzser123@gmail.com>
2021-05-15 05:14:11 +0900

    20210515-newpost

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -4,4 +4,10 @@ var store = [{
         "tags": ["ML_Agents","Unity"],
         "url": "/ml_agents/mlagent1/",
         "teaser": null
+      },{
+        "title": "유니티 ML_Agents 공부해보기 - 기본구조(2) Brain",
+        "excerpt":"0. 개요 지난번에는 주인공 에이전트에 대해서 알아봤다면 이번에는 브레인에 대해서 알아볼 것이다. 브레인은 유니티 블로그에서는 다음과 같이 설명하고 있다. “각 브레인은 특정 상태와 행동 공간을 정의하고, 연결된 에이전트가 어떤 행동을 취할지 결정합니다.” 브레인은 현재 릴리즈를 기점으로 총 4가지 모드를 지원한다. 마찬가지로 유니티 블로그에서는 다음과 같이 설명하고 있다. ● External :...","categories": ["ML_Agents"],
+        "tags": ["ML_Agents","Unity"],
+        "url": "/ml_agents/mlagent2/",
+        "teaser": null
       }]
