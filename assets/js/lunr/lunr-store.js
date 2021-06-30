4665a59f4b685e4349e3bba73409439cae9f73d2
hrabit <hzser123@gmail.com>
2021-06-30 17:16:18 +0900

    2021-06-30-신규포스트

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -16,4 +16,16 @@ var store = [{
         "tags": ["ML_Agents","Unity"],
         "url": "/ml_agents/mlagent3/",
         "teaser": null
+      },{
+        "title": "유니티 ML_Agents 공부해보기 - 기본구조(3) Academy",
+        "excerpt":"0. 기본 구조 유니티 블로그에서는 아카데미를 다음과 같이 설명하고 있다. “특정 씬에 있는 아카데미 오브젝트는 해당 환경에 포함된 모든 브레인을 자식으로 포함하고 있습니다. 각 환경은 환경의 범위를 정의하는 한 개의 아카데미를 포함하고 있으며, 그 정의는 다음과 같은 내용을 포함합니다.” 이 아카데미가 포함하는 항목은 다음과 같다. ● 브레인 설정 ● 화면...","categories": ["ML_Agents"],
+        "tags": ["ML_Agents","Unity"],
+        "url": "/ml_agents/mlagent4/",
+        "teaser": null
+      },{
+        "title": "케라스 튜너를 이용해 하이퍼 파라미터를 최적화 해보자!",
+        "excerpt":"0. 개요 이 글에서 작성한 코드는 Git(https://github.com/hrabit64/keras_tuner_exercise)에 올려져 있습니다! 머신러닝을 공부하다 보면 하이퍼 파라미터란 용어를 듣게 될 것이다. 구글의 Tensorflow 문서에서는 이 하이퍼파리미터를 “훈련 프로세스 및 ML 모델의 토폴로지를 제어하는 변수입니다.” 라고 정의하고 있다. 쉽게 말해 파라미터는 ML이 결정하는 요소이고, 하이퍼 파라미터는 사용자가 정해줘야 하는 요소라고 생각하면 된다. 이 하이퍼...","categories": ["Tensorflow"],
+        "tags": ["Tensorflow","keras-tuner"],
+        "url": "/tensorflow/keras-tuner/",
+        "teaser": null
       }]
